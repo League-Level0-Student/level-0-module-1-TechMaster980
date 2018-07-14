f99c187c999e40e00494666df97c72c31d433cdb
league <league@wts7.attlocal.net>
2018-07-14 12:58:19 -0700

    riddler xgeeks

--- src/TheRiddler.java
+++ src/TheRiddler.java
@@ -12,14 +12,23 @@ public class TheRiddler {
 		// 1. Make a variable to hold the score
         int score= 0; 
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+         String answer=JOptionPane.showInputDialog( "Which creature walks on four legs in the morning, two legs in the afternoon, and three legs in the evening?");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+        if(answer.equals("man")) {
+         score++;
+         JOptionPane.showMessageDialog(null, "correct");}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+        else {JOptionPane.showMessageDialog(null, "wrong");}
 		// 6. Add some more riddles
-
-		// 2. Make a pop up to show the score.
+        String answer2=JOptionPane.showInputDialog( "What has a head and a tail, but no body?");
+		if(answer2.equals("coin")) {
+			score++;
+			JOptionPane.showMessageDialog(null, "correct");
+			String answer3=JOptionPane.showInputDialog( "What has an eye but can not see?");
+			if(answer3.equals("needle"));
+			score++;
+		}
+        // 2. Make a pop up to show the score.
 		JOptionPane.showMessageDialog(null, score);
 	}
 }
--- src/XGeeks.java
+++ src/XGeeks.java
@@ -15,11 +15,13 @@ public class XGeeks {
 public static void main(String[] args) {
 
 		// 1. Save the superpower for each person in a variable.
-
+		String kevinpower="doing codes";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+				String name=JOptionPane.showInputDialog("what is your power");
 		// 3. Show the superpower in a pop-up, depending on the name entered. 
-
+		if(name.equals("kevin")) {
+		JOptionPane.showMessageDialog(null, kevinpower);
+		}
 	}
 }
 
