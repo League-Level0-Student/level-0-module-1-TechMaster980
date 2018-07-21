43836cc0a73abd08925046ac0c59303c2100a360
league <league@wts5.attlocal.net>
2018-07-21 12:56:18 -0700

    hard

---
+++ src/badger.java
@@ -0,0 +1,11 @@
+
+public class badger {
+public static void main(String[] args) {
+	
+	for(int i=0; i<10; i++) {
+		System.out.print(" badger,");
+	for(int i=0; i<10; i++) {
+		System.out.print(" mushroom,");
+	}
+}
+}
