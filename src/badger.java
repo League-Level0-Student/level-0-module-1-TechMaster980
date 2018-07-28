7ffcf0d1b0400431d1c8605d5cc1dbf676199cbe
league <league@imac-4.attlocal.net>
2018-07-28 12:58:35 -0700

    so hard did badger still in progress

--- src/badger.java
+++ src/badger.java
@@ -1,11 +1,17 @@
 
 public class badger {
-public static void main(String[] args) {
-	
-	for(int i=0; i<10; i++) {
-		System.out.print(" badger,");
-	for(int i=0; i<10; i++) {
-		System.out.print(" mushroom,");
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+			System.out.print(" badger,");
+			if (i == 9) {
+				for (int i1 = 0; i1 < 2; i1++) {
+					System.out.print(" mushroom,");
+				}
+			}
+			for (int i1 = 0; i1 < 3; i1++) {
+				System.out.print(" snake, ");
+			}
+		}
 	}
-}
 }
