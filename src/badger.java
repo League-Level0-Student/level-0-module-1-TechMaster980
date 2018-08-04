c335939f57e35db9a6afe980c35cc0555565831f
league <league@wts14-4.attlocal.net>
2018-08-04 12:53:22 -0700

    just finished badger

--- src/badger.java
+++ src/badger.java
@@ -1,16 +1,18 @@
 
 public class badger {
 	public static void main(String[] args) {
-
-		for (int i = 0; i < 10; i++) {
-			System.out.print(" badger,");
-			if (i == 9) {
-				for (int i1 = 0; i1 < 2; i1++) {
-					System.out.print(" mushroom,");
+		for (int b = 0; b < 10; b++) {
+			for (int i = 0; i < 10; i++) {
+				System.out.print(" badger,");
+				if (i == 9) {
+					for (int i1 = 0; i1 < 2; i1++) {
+						System.out.print(" mushroom,");
+					}
 				}
 			}
 			for (int i1 = 0; i1 < 3; i1++) {
 				System.out.print(" snake, ");
+
 			}
 		}
 	}
